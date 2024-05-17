e0080f16e67da35c786d918168fec9a6ff20fae3
Mohib <mohibullahmohim2020@gmail.com>
2024-05-17 11:49:02 +0600

    Readme file and some page and component added

---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div>
+            <h1> i am from Footer  </h1>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1> I am from Home </h1>
+        </div>
+    );
+};
+
+export default Home;
