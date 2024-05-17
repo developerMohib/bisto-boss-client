499af3f533c78d9d1c1cb1632390adeac5b6ba64
Mohib <mohibullahmohim2020@gmail.com>
2024-05-17 12:12:05 +0600

    Root added

---
+++ src/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+
+import { Link } from 'react-router-dom';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1> I ma error page </h1>
+            <Link to='/' > beck home </Link>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/Root/Root.jsx
@@ -0,0 +1,17 @@
+
+import { Outlet } from 'react-router-dom';
+import Navbar from '../Component/Navbar/Navbar';
+import Footer from '../Component/Footer/Footer';
+
+const Root = () => {
+    return (
+        
+        <div>
+            <Navbar> </Navbar>
+            <Outlet> </Outlet>
+            <Footer> </Footer>
+        </div>
+    );
+};
+
+export default Root;
