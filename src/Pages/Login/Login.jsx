d8fca12743537dcbec5a93590a7cb23fe6169047
Mohib <mohibullahmohim2020@gmail.com>
2024-05-23 10:50:36 +0600

    log in from added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,10 +4,100 @@ import DynamicTitle from "../../Component/DynamicTitle/DynamicTitle";
 const Login = () => {
   return (
     <div>
-      <DynamicTitle titleName={'Login'}></DynamicTitle>
-      <p> i m login page </p>
-      <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est, aliquid labore quas blanditiis doloremque culpa amet sapiente accusamus eum tempora beatae dolorum placeat molestiae quis, rerum exercitationem ducimus ipsum nostrum dignissimos? Quidem similique incidunt quaerat, totam commodi facere temporibus cupiditate repellat accusamus animi nulla repudiandae ullam ad laborum nesciunt ab velit possimus quis! Obcaecati ipsam nisi iste perferendis quidem temporibus alias non possimus corporis incidunt ullam, sit veritatis nesciunt totam dicta voluptatibus dignissimos placeat fugit cum adipisci error cumque! Fugit odit aperiam, harum facere blanditiis eligendi cumque id nemo placeat! Sed ratione ad voluptatem soluta itaque nostrum rerum. Voluptatum, illo? </p>
-      <button className='btn btn-outline' ><Link to='/register' > Register </Link></button>
+      <DynamicTitle titleName={"Login"}></DynamicTitle>
+
+      <div className="flex flex-wrap min-h-screen w-full content-center justify-center bg-gray-200 py-10">
+          
+
+        <div className="mt-3 w-full">
+          <p className="text-center font-bold text-3xl mb-10 ">
+            Please Login {" "}
+          </p>
+        </div>
+        <div className="flex shadow-md">
+          <div
+            className="flex flex-wrap content-center justify-center rounded-l-md bg-white"
+          style= {{width: '24rem', height: '32rem'}} 
+          >
+            <div className="w-72">
+              <h1 className="text-xl font-semibold">Welcome back</h1>
+              <small className="text-gray-400">
+                Welcome back! Please enter your details
+              </small>
+
+              <form className="mt-4">
+                <div className="mb-3">
+                  <label className="mb-2 block text-xs font-semibold">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label className="mb-2 block text-xs font-semibold">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="*****"
+                    className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+                  />
+                </div>
+
+                <div className="mb-3 flex flex-wrap content-center">
+                  <input
+                    id="remember"
+                    type="checkbox"
+                    className="mr-1 checked:bg-purple-700"
+                  />{" "}
+                  <label
+                    className="mr-auto text-xs font-semibold"
+                  >
+                    Remember for 30 days
+                  </label>
+                  <a href="#" className="text-xs font-semibold text-purple-700">
+                    Forgot password?
+                  </a>
+                </div>
+
+                <div className="mb-3">
+                  <button className="mb-1.5 block w-full text-center text-white bg-purple-700 hover:bg-purple-900 px-2 py-1.5 rounded-md">
+                    Sign in
+                  </button>
+                  <button className="flex flex-wrap justify-center w-full border border-gray-300 hover:border-gray-500 px-2 py-1.5 rounded-md">
+                    <img
+                      className="w-5 mr-2"
+                      src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA"
+                    />
+                    Sign in with Google
+                  </button>
+                </div>
+              </form>
+
+              <div className="text-center">
+                <span className="text-xs text-gray-400 font-semibold">
+                  Dont have account?
+                </span>
+                <Link className="text-xs font-semibold text-purple-700" to="/register"> Sign up </Link>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className="flex flex-wrap content-center justify-center rounded-r-md"
+            style={{width: '24rem', height: '32rem'}}
+          >
+            <img
+              className="w-full h-full bg-center bg-no-repeat bg-cover rounded-r-md"
+              src="https://i.imgur.com/9l1A4OS.jpeg"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,15 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../Component/Navbar/Navbar";
 import Footer from "../Component/Footer/Footer";
 
 const Root = () => {
+  const location = useLocation() ;
+  const noHeaderFooter = location.pathname.includes('login') ;
+
   return (
     <div>
-      <Navbar> </Navbar>
+      { noHeaderFooter || <Navbar> </Navbar> }
       <div className="max-w-screen-xl mx-auto" >
         <Outlet> </Outlet>
       </div>
-      <Footer> </Footer>
+      { noHeaderFooter || <Footer> </Footer> }
     </div>
   );
 };
