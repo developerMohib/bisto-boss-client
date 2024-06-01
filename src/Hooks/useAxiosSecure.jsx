aa50095a5fd74c4557eef81e8f131ef45f1addc0
Mohib <mohibullahmohim2020@gmail.com>
2024-06-01 13:15:52 +0600

    check out route create with bug fixxing

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -26,7 +26,7 @@ const useAxiosSecure = () => {
       return response;
     },
     async function (error) {
-        const status = error.response.status ;
+        const status = error.response?.status ;
         if(status === 401 || status === 403){
             // log out kore dibo and log in page a jaw
             await logOut()
--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,10 +1,25 @@
 import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import useCartData from "../../../Hooks/useCartData";
 const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const axiosSecure = useAxiosSecure();
   const [errorMessage, setErrorMessage] = useState(" ");
+  const [clientSecret, setClientSecret] = useState('')
+  const [cart] = useCartData();
+  const price = cart.reduce((total, item) => total + item.price, 0);
+  console.log("price ", price);
+
+  //
+  useEffect(() => {
+    axiosSecure.post("/create-payment-intent", { price }).then((res) => {
+      console.log(res.data);
+      setClientSecret(res.data.clientSecret)
+    });
+  }, [axiosSecure, price]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -12,7 +27,6 @@ const CheckoutForm = () => {
     if (!stripe || !elements) {
       return;
     }
-
     if (elements == null) {
       return;
     }
@@ -28,9 +42,9 @@ const CheckoutForm = () => {
     if (error) {
       setErrorMessage(error.message);
       console.log("what error", error);
-      toast.error(error.message)
+      toast.error(error.message);
     } else {
-      console.log("payment method", paymentMethod);
+      // console.log("payment method", paymentMethod);
       toast.success("your payment successfully");
       setErrorMessage(" ");
     }
@@ -58,7 +72,7 @@ const CheckoutForm = () => {
         <button
           className="btn btn-outline"
           type="submit"
-          disabled={!stripe || !elements}
+          disabled={!stripe || !elements || !clientSecret}
         >
           Pay
         </button>
